f0097942e991d414a38cb83e5cfd44ed841f6d52
Emanuel <e.rista@outlook.it>
2020-11-03 16:15:10 +0100

    comments

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ $(".toggle_btn").click(
   function() {
     $(".tenda").slideToggle(3000, function(){
       alert("Lo spettacolo è cominciato!")
-    })//3000 = 3 secondi
+    })//3000 = 3 secondi + fynction etc = callback
   }
 );
